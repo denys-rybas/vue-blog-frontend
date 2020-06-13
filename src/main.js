1e13ac6c332ed5ea0cb24a0928b9da083b88f037
“Dennis.r.ysbm” <android6636@gmail.com>
2020-06-13 11:59:31 +0300

    env, axios, scss, vue-bootstrap

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import axios from "axios";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import "./assets/styles/app.scss"
 
 Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
