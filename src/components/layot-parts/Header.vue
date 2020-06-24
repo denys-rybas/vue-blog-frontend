2b5e6b875aedd4ddf6455dd5756f1d6caad9155a
“Dennis.r.ysbm” <android6636@gmail.com>
2020-06-24 17:31:29 +0300

    added logout

--- src/components/layot-parts/Header.vue
+++ src/components/layot-parts/Header.vue
@@ -1,35 +1,45 @@
 <template>
-    <div>
-        <b-navbar toggleable="lg" type="dark" variant="info">
-            <b-container>
-                <b-navbar-brand href="#">
-                    Blog
-                </b-navbar-brand>
+	<div>
+		<b-navbar toggleable="lg" type="dark" variant="info">
+			<b-container>
+				<b-navbar-brand href="#">
+					Blog
+				</b-navbar-brand>
 
-                <b-navbar-toggle target="nav-collapse"/>
+				<b-navbar-toggle target="nav-collapse"/>
 
-                <b-collapse id="nav-collapse" is-nav>
-                    <b-navbar-nav>
-                        <router-link to="/" class="p-2 text-light">Home</router-link>
+				<b-collapse id="nav-collapse" is-nav>
+					<b-navbar-nav>
+						<router-link to="/" class="p-2 text-light">Home</router-link>
 
-                        <router-link to="/about" class="p-2 text-light">About</router-link>
-                    </b-navbar-nav>
+						<router-link to="/about" class="p-2 text-light">About</router-link>
+					</b-navbar-nav>
 
-                    <!-- Right aligned nav items -->
-                    <b-navbar-nav class="ml-auto">
+					<!-- Right aligned nav items -->
+					<b-navbar-nav class="ml-auto">
+						<b-button variant="light"
+						          @click="logout()"
+						>
+							Log out
+						</b-button>
+					</b-navbar-nav>
+				</b-collapse>
+			</b-container>
 
-                    </b-navbar-nav>
-                </b-collapse>
-            </b-container>
-
-        </b-navbar>
-    </div>
+		</b-navbar>
+	</div>
 </template>
 
 <script>
-    export default {
-        name: "Header"
-    }
+	export default {
+		name: "Header",
+      methods: {
+          logout() {
+              localStorage.removeItem('access_token');
+              this.$router.push('/login');
+          }
+      }
+	}
 </script>
 
 <style scoped>
